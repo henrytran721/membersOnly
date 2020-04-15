ff378fa05c5d8b836306cee222387dea3e509ee9
Henry Tran <henrytran@Henry-Trans-MacBook-Pro.local>
2020-04-14 23:23:41 -0700

    added different port

--- app.js
+++ app.js
@@ -160,6 +160,6 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('App is listening at port 3000');
 })
